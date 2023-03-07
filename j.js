384f04310ba46ab8b8dc3584f92e8cc2e11fcd6e
mariamismaiel <76557785+mariamismaiel@users.noreply.github.com>
2023-03-07 03:44:45 +0200

    Add files via upload

---
+++ j.js
@@ -0,0 +1,15 @@
+function changeBodyBg(pic){
+var image = 'url(' + pic + '.jpg)';
+localStorage.setItem("myimage",image);
+var a=localStorage.getItem("myimage");
+document.body.style.backgroundImage= a;
+    }
+
+function changeBackground()
+{
+var a=localStorage.getItem("myimage");
+document.body.style.background= a;
+}
+function playAudio(x) {
+ document.getElementById(x).play();
+}
